b482fc42af1991e3be8818e64b03a9d67eb19bde
Sebastian J. Mielke <sjm@sjmielke.com>
2014-07-24 23:08:07 +0200

    Stupid parsing is stupid. Then again, not caring about complicated cases still works well enough to get *some* data to have fun with :)

--- hasrbash.hs
+++ hasrbash.hs
@@ -1,9 +1,15 @@
 import Text.XML.Light
 import Network.HTTP
 import Data.Maybe (catMaybes, fromJust)
-import Control.Monad (liftM)
+import Control.Monad (liftM2)
 import Text.HTML.TagSoup.Entity (lookupEntity)
 import Codec.Binary.UTF8.String (decodeString)
+import Text.Parsec
+import Text.Parsec.Text
+import Data.Text (pack)
+import Data.Either (rights)
+import Data.List (sort, group, sortBy)
+import Data.Ord (comparing)
 
 unescapeEntities :: String -> String
 unescapeEntities [] = []
@@ -28,6 +34,25 @@ getRSSChannel = fromJust . findElement (unqual "channel") . head . tail . onlyEl
 getQuotes :: [Content] -> [String]
 getQuotes = map (stripToStringLines . elContent) . findElements (unqual "description") . getRSSChannel
 
-main = readFile ".conf"
-       >>= fetch
-       >>= mapM_ putStrLn . getQuotes . parseXML
+onlySimpleQuotes :: [String] -> [String]
+onlySimpleQuotes = filter (\s -> head s == '"' && not ('\n' `elem` s))
+
+--------------------------------------------------------------------------------
+
+myParse :: String -> Either ParseError (String, String)
+myParse = parse quoteDefinition "(rss feed)" . pack . reverse
+
+quoteDefinition :: GenParser state (String, String)
+quoteDefinition = do author <- many1 (noneOf " - \"")
+                     try (string " - \"")
+                     text <- many1 (noneOf "\"")
+                     char '"'
+                     return (reverse author, reverse text)
+
+--------------------------------------------------------------------------------
+
+main = do rss <- readFile ".conf" >>= fetch
+          let quotes = rights . map myParse . onlySimpleQuotes . getQuotes . parseXML $ rss
+          mapM_ print quotes
+          print $ length quotes
+          print $ reverse . sortBy (comparing snd) . map (liftM2 (,) head length) . group . sort $ map fst quotes
