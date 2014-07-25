1122cc0a6cc3493ed7b6499c124a28a1adfd01b5
Sebastian J. Mielke <sjm@sjmielke.com>
2014-07-25 15:24:27 +0200

    Slowly getting the hang of Parsec, has some nice helper functions. All Single-Line-Quotes except for one are working now - and that one has a really bad format... (Kilian, fix it NAO) (Oh, and do I already see hints about Multi-Line-Quote parsing there?)

--- hasrbash.hs
+++ hasrbash.hs
@@ -7,9 +7,11 @@ import Codec.Binary.UTF8.String (decodeString)
 import Text.Parsec
 import Text.Parsec.Text
 import Data.Text (pack)
-import Data.Either (rights)
+import Data.Either (lefts, rights)
 import Data.List (sort, group, sortBy)
 import Data.Ord (comparing)
+import Data.Char (isSpace)
+import Control.Applicative ((<$>), (<*), (*>))
 
 unescapeEntities :: String -> String
 unescapeEntities [] = []
@@ -40,19 +42,33 @@ onlySimpleQuotes = filter (\s -> head s == '"' && not ('\n' `elem` s))
 --------------------------------------------------------------------------------
 
 myParse :: String -> Either ParseError (String, String)
-myParse = parse quoteDefinition "(rss feed)" . pack . reverse
+myParse q = parse quoteDefinition (" »"++q++"« ") . pack . reverse $ q
 
 quoteDefinition :: GenParser state (String, String)
-quoteDefinition = do author <- many1 (noneOf " - \"")
-                     try (string " - \"")
-                     text <- many1 (noneOf "\"")
-                     char '"'
-                     return (reverse author, reverse text)
+quoteDefinition = (try singleQuote) -- <|> multiQuote
+
+singleQuote :: GenParser state (String, String)
+singleQuote = do author <- manyTill anyChar trySpacedDash
+                 many space
+                 char '"'
+                 text <- manyTill anyChar (try $ char '"' >> eof)
+                 return (trim . reverse $ author, reverse text)
+    where
+        trim s = if isSpace $ head s then tail s else s
+        dash = char '-' <|> char '\8211'
+        trySpacedDash = (try $ dash >> space) <|> (try $ space >> dash) <?> "author separator (dash with at least one space)"
+
+multiQuote :: GenParser state (String, String)
+multiQuote = do text <- many1 (noneOf "")
+                return ("*Multi", reverse text)
 
 --------------------------------------------------------------------------------
 
 main = do rss <- readFile ".conf" >>= fetch
-          let quotes = rights . map myParse . onlySimpleQuotes . getQuotes . parseXML $ rss
+          let parsed = map myParse . onlySimpleQuotes . getQuotes . parseXML $ rss
+          let quotes = rights parsed
           mapM_ print quotes
           print $ length quotes
           print $ reverse . sortBy (comparing snd) . map (liftM2 (,) head length) . group . sort $ map fst quotes
+          mapM_ print $ filter ((=="*Multi") . fst) quotes
+          mapM_ print $ lefts parsed
